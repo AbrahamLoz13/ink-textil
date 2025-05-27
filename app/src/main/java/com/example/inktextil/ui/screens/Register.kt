4fb579ee519388e7ab92af752c746a1d51d569b6
alucardabyuwu <anahydrlira@gmail.com>
2025-05-26 22:32:35 -0600

    a

--- app/src/main/java/com/example/inktextil/ui/screens/Register.kt
+++ app/src/main/java/com/example/inktextil/ui/screens/Register.kt
@@ -142,7 +142,7 @@ fun RegisterScreen(navController: NavHostController) {
                                 val nuevoUsuario = UsuarioData(
                                     username = usernameText,
                                     correo = emailText
-                                    // el resto de los campos quedan como por defec
+                                    // el resto de los campos quedan como por defe
                                 )
 
                                 if (uid != null) {
